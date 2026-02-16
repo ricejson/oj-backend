be7a81320e7b11eb32f54dc1eecc3e1b649f5801
ricejson <wjjbangbangbang@163.com>
2026-02-16 22:35:26 +0800

    补充题目提交记录数据模型

--- repository/dao/submit/question_submit.go
+++ repository/dao/submit/question_submit.go
@@ -1,1 +1,36 @@
 package submit
+
+import (
+	"context"
+
+	"gorm.io/gorm"
+)
+
+type QuestionSubmitDAO struct {
+	db *gorm.DB
+}
+
+func NewQuestionSubmitDAO(db *gorm.DB) *QuestionSubmitDAO {
+	return &QuestionSubmitDAO{
+		db: db,
+	}
+}
+
+// Insert 插入一条题目提交记录
+func (dao *QuestionSubmitDAO) Insert(ctx context.Context, questionSubmit QuestionSubmit) (int64, error) {
+	err := dao.db.WithContext(ctx).Create(&questionSubmit).Error
+	if err != nil {
+		return 0, err
+	}
+	return questionSubmit.Id, nil
+}
+
+// FindById 查询id查询题目
+func (dao *QuestionSubmitDAO) FindById(ctx context.Context, id int64) (QuestionSubmit, error) {
+	var questionSubmit QuestionSubmit
+	err := dao.db.WithContext(ctx).
+		Model(&QuestionSubmit{}).
+		Where("id = ?", id).
+		Find(&questionSubmit).Error
+	return questionSubmit, err
+}
--- repository/dao/submit/types.go
+++ repository/dao/submit/types.go
@@ -6,13 +6,13 @@ import (
 
 // QuestionSubmit 题目提交表
 type QuestionSubmit struct {
-	ID         int64     `gorm:"column:id;type:bigint;primaryKey;autoIncrement;comment:题目提交唯一标识" json:"id"`
-	QuestionID int64     `gorm:"column:question_id;type:bigint;not null;index:idx_question_id;comment:题目id" json:"question_id"`
+	Id         int64     `gorm:"column:id;type:bigint;primaryKey;autoIncrement;comment:题目提交唯一标识" json:"id"`
+	QuestionId int64     `gorm:"column:question_id;type:bigint;not null;index:idx_question_id;comment:题目id" json:"question_id"`
 	Code       string    `gorm:"column:code;type:text;not null;comment:提交代码" json:"code"`
 	Language   string    `gorm:"column:language;type:varchar(64);not null;comment:语言（JAVA、Go等）" json:"language"`
 	Status     uint8     `gorm:"column:status;type:tinyint;default:0;comment:判题结果 1-等待中 2-失败 3-成功 4-系统错误" json:"status"`
 	JudgeInfo  string    `gorm:"column:judge_info;type:varchar(128);comment:判题信息JSON字符串" json:"judge_info"`
-	UserID     int64     `gorm:"column:user_id;not null;index:idx_user_id;comment:提交用户id" json:"user_id"`
+	UserId     int64     `gorm:"column:user_id;not null;index:idx_user_id;comment:提交用户id" json:"user_id"`
 	CreatedAt  time.Time `gorm:"column:create_at;type:datetime;default:CURRENT_TIMESTAMP;index:idx_create_at;comment:创建时间" json:"create_at"`
 	UpdatedAt  time.Time `gorm:"column:update_at;type:datetime;default:CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP;comment:更新时间" json:"update_at"`
 	IsDelete   int8      `gorm:"column:is_delete;type:tinyint;default:0;comment:是否被删除（逻辑删除）0-未删除，1-删除" json:"is_delete"`
